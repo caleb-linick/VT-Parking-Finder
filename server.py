e81a344e269efe56c598117fd081a826c0f6d565
Michael Pang <46881699+HiMichaelPang@users.noreply.github.com>
2025-03-13 18:26:28 -0400

    Update server.py
    Added update occupancy functionality

--- server.py
+++ server.py
@@ -8,6 +8,11 @@ mydb = database_module.connectdatabase()
 
 session = {}
 
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+
 @app.route('/user_auth')
 def user_auth():
     return render_template('user_auth.html')
@@ -23,7 +28,7 @@ def login():
     password = request.form['password']
     if database_module.authenticate(mydb, username, password):
         session['username'] = username
-        return redirect(url_for('index')) # redirect to a user page?
+        return redirect(url_for('update')) # redirect to a user page?
     else:
         return "Incorrect Password or Username. Try again", 401
 
@@ -37,7 +42,7 @@ def signup():
     try:
         if database_module.signup_user(mydb, username, password):
             session['username'] = username
-            return redirect(url_for('index')) # redirect to a user page?
+            return redirect(url_for('update')) # redirect to a user page?
         else:
             return "Signup Failed", 400
     except mysql.connector.IntegrityError as e:
@@ -49,6 +54,18 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+
+@app.route('/occupancy', methods=['PUT'])
+def update_occupancy():
+    
+    if request.method == 'PUT':
+        print('inside')
+        data = request.data
+        # spot_id = request.form["id"]
+        # spot_occupancy = request.form['occupancy']
+        database_module.update_entity(mydb, data)
+            
+        return "Data is updated"
 
 
 if __name__ == '__main__':
